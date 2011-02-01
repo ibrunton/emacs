681c193016da25e2597995d1e0c3379bc24bffc4
ibrunton <wolfshift@gmail.com>
2011-02-01 19:37:06 -0400

    Set povray include dir

--- idb-pov.el
+++ idb-pov.el
@@ -3,6 +3,8 @@
 ;; POV-RAY MODE
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(setq pov-include-dir "/usr/share/povray-3.6/include")
+
 (autoload 'pov-mode "pov-mode" "POV-Ray scene file mode" t)
 (add-to-list 'auto-mode-alist '("\\.pov\\'" . pov-mode))
 (add-to-list 'auto-mode-alist '("\\.inc\\'" . pov-mode))
